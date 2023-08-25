ce81632ed1540cfc4c5032d0f11af96878ba0cc4
Iamneo <techadmin@iamneo.ai>
2023-08-25 12:00:00 +0000

    Backup Commit

--- reactapp/src/functions/destructuring.js
+++ reactapp/src/functions/destructuring.js
@@ -1,10 +1,10 @@
-export default function getUserIpDetails(userResponse={}){
+export function getUserIpDetails(userResponse={}){
 
     const  {userName,userIp='0.0.0.0'}=userResponse;
     return [userName,userIp];
 }
 
-export default function getTopThree(studentMarks=[]){
+export function getTopThree(studentMarks=[]){
     
     const [first=null, second=null,third=null,...rest]=studentMarks;
     return [first,second,third];
