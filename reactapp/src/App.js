2ec46301db6196dfd1380d2a58bc9f84d6b54ecf
Iamneo <techadmin@iamneo.ai>
2023-08-25 12:38:19 +0000

    Backup Commit

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -2,10 +2,22 @@ import {getUserIpDetails, getTopThree} from './functions/destructuring';
 function App(){
   return(
     <>
-    <getUserIpDetails/>
-    <getTopThree/>
+    
     </>
 
   );
 }
+export default App;
+
+
+function App() {
+  return (
+    <div className="App">
+    
+     <getUserIpDetails/>
+     <getTopThree/>
+    </div>
+  );
+}
+
 export default App;
